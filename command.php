e75def893444c9e9dafc17b5258d4c3edfdfd684
mcdruid <drew@mcdruid.co.uk>
2019-12-02 14:49:26 +0000

    Cleaning up and reformatting

--- command.php
+++ command.php
@@ -1,94 +1,112 @@
 <?php
 
-if ( ! class_exists( 'WP_CLI' ) ) {
-	return;
-}
-
 /**
- * Try to crack user passwords
- *
- * @when after_wp_load
+ * Command to crack user passwords.
+ * Based on https://www.drupal.org/project/drop_the_ripper
  */
-$wapuu_the_ripper_command = function() {
-  $users = get_users();
+Class Wapuu_The_Ripper_Command {
 
-  $wordlist = __DIR__ . '/wordlist.txt';
-  $passwords = _dtr_load_wordlist($wordlist);
-  $matches = [];
-  $checked = 0;
+	/**
+	 * Try to crack user passwords
+	 *
+	 * Users are filtered / selcted via arguments supported by
+	 * [WP_User_Query()](https://developer.wordpress.org/reference/classes/wp_user_query/prepare_query/).
+	 *
+	 * ## OPTIONS
+	 *
+	 * [--role=<role>]
+	 * : Only display users with a certain role.
+	 *
+	 * [--top=<top>]
+	 * : Use the top x passwords from the wordlist.
+	 *
+	 * [--all]
+	 * : Use all of the passwords from the wordlist.
+	 */
+	public function __invoke($args, $assoc_args) {
 
-  foreach($users as $user) {
-    $checked ++;
-    foreach($passwords as $password) {
-      if (wp_check_password($password, $user->user_pass, $user->ID)) {
-        $matches[] = $user;
-        WP_CLI::warning('Match! ' . $user->ID . ': ' . $user->data->user_login . ' password: ' . $password);
-        continue;
-      }
-    }
-  }
+		$users = get_users($assoc_args);
 
-  if (empty($matches)) {
-    WP_CLI::success('Checked ' . $checked . ' users. No matches.');
-  }
-  else {
-    WP_CLI::success('Checked ' . $checked . ' users. ' . count($matches) . ' match(es).');
-  }
-};
-WP_CLI::add_command( 'wtr', $wapuu_the_ripper_command );
+		$wordlist = __DIR__ . '/wordlist.txt';
+		$passwords = $this->wtr_load_wordlist($wordlist, $assoc_args);
+		$matches = [];
+		$checked = 0;
 
-function drush_get_option($name, $default) {
-  return $default;
+		foreach ($users as $user) {
+			$checked++;
+			foreach ($passwords as $password) {
+				if (wp_check_password($password, $user->data->user_pass, $user->ID)) {
+					$matches[] = $user;
+					WP_CLI::warning('Match! ' . $user->ID . ': ' . $user->data->user_login . ' password: ' . $password);
+					continue;
+				}
+			}
+		}
+
+		if (empty($matches)) {
+			WP_CLI::success('Checked ' . $checked . ' users. No matches.');
+		}
+		else {
+			WP_CLI::success('Checked ' . $checked . ' users. ' . count($matches) . ' match(es).');
+		}
+	}
+
+	/**
+	 * Parse a wordlist file into an array.
+	 *
+	 * @param string $wordlist
+	 *   Path to the wordlist file.
+	 *
+	 * @return array
+	 *   Candidate passwords.
+	 */
+	private function wtr_load_wordlist($wordlist, $assoc_args) {
+		$passwords = file($wordlist);
+		$passwords = array_filter($passwords, [
+			$this,
+			'wtr_wordlist_filter_callback',
+		]);
+		$passwords = array_map([$this, 'wtr_trim_newline'], $passwords);
+		$passwords = array_unique($passwords);
+
+		if (!isset($assoc_args['all'])) {
+			$top = isset($assoc_args['top']) ? (int) $assoc_args['top'] : 25;
+			if ($top > 0) {
+				$passwords = array_slice($passwords, 0, $top);
+			}
+		}
+
+		return $passwords;
+	}
+
+	/**
+	 * Callback for wordlist array filtering; removes comments.
+	 *
+	 * @param string $line
+	 *   An item from a wordlist.
+	 *
+	 * @return bool
+	 *   FALSE if the line is a comment.
+	 */
+	private function wtr_wordlist_filter_callback($line) {
+		return (strpos($line, '#!comment:') !== 0);
+	}
+
+	/**
+	 * Callback for wordlist array trimming; remove only trailing newlines.
+	 *
+	 * @param string $line
+	 *   An item from a wordlist.
+	 *
+	 * @return string
+	 *   Candidate password with trailing newline removed.
+	 */
+	private function wtr_trim_newline($line) {
+		// Note that double quotes are necessary for the whitespace characters.
+		return rtrim($line, "\r\n");
+	}
+
 }
 
-/**
- * Parse a wordlist file into an array.
- *
- * @param string $wordlist
- *   Path to the wordlist file.
- *
- * @return array
- *   Candidate passwords.
- */
-function _dtr_load_wordlist($wordlist) {
-  $passwords = file($wordlist);
-  $passwords = array_filter($passwords, '_dtr_wordlist_filter_callback');
-  $passwords = array_map('_dtr_trim_newline', $passwords);
-  $passwords = array_unique($passwords);
-
-  if (!drush_get_option('all', FALSE)) {
-    $top = (int) drush_get_option('top', 25);
-    if ($top > 0) {
-      $passwords = array_slice($passwords, 0, $top);
-    }
-  }
-
-  return $passwords;
-}
-
-/**
- * Callback for wordlist array filtering; removes comments.
- *
- * @param string $line
- *   An item from a wordlist.
- *
- * @return bool
- *   FALSE if the line is a comment.
- */
-function _dtr_wordlist_filter_callback($line) {
-  return (strpos($line, '#!comment:') !== 0);
-}
-
-/**
- * Callback for wordlist array trimming; remove only trailing newlines.
- *
- * @param string $line
- *   An item from a wordlist.
- *
- * @return string
- *   Candidate password with trailing newline removed.
- */
-function _dtr_trim_newline($line) {
-  // Note that double quotes are necessary for the whitespace characters.
-  return rtrim($line, "\r\n");
-}
+WP_CLI::add_command('wtr', 'Wapuu_The_Ripper_Command'); // Struggling with @alias
+WP_CLI::add_command('wapuu_the_ripper', 'Wapuu_The_Ripper_Command');
